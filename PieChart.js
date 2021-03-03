11a6ff5b513d45b17919945a8c663c1294d67fb4
Szelma <raduniek1@gmail.com>
2021-03-03 21:23:57 +0100

    Update PieChart.js

--- PieChart.js
+++ PieChart.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from "react";
 
-export const PieChart2 = ({ initialValues = [15, 31, 221, 5, 15] }) => {
-  const cx = 80;
-  const cy = 80;
-  const radius = 60;
+import './PieChart.css'
+
+export const PieChart = ({ initialValues = [15, 31, 221, 5, 15] }) => {
+  const cx = 125;
+  const cy = 125;
+  const radius = 80;
   const strokeWidth = 30;
-  const colors = ["#6495ED", "purple", "#cd5c5c", "blue", "lightgreen", "orange", "teal"];
+  const colors = [
+    "#6495ED",
+    "purple",
+    "#cd5c5c",
+    "blue",
+    "lightgreen",
+    "orange",
+    "teal",
+  ];
   const circumference = 2 * Math.PI * radius;
   const adjustedCircumference = circumference - 6;
   let sortedValues = [];
@@ -14,42 +24,72 @@ export const PieChart2 = ({ initialValues = [15, 31, 221, 5, 15] }) => {
   let chartData = [];
   const [circleTransform, setCircleTransform] = useState([]);
   const [strokeDashOffset, setStrokeDashOffset] = useState([]);
+  const [hoveredItems, setHoveredItems] = useState([]);
+  const [chartValues, setChartValues] = useState([]);
+
+  //hover className="onhover"  {circleTransform[index]} and scale
 
   useEffect(() => {
-    sortedValues = initialValues.sort((a, b) => b - a);
-    dataTotal = sortedValues.reduce((acc, val) => acc + val)
-    sortedValues.forEach((dataVal, index) => {
-      chartData.push(angleOffset)
-      angleOffset = dataVal / dataTotal * 360 + angleOffset;
-    })
+    const size = initialValues.length;
+    let items = []
+    for (let i=0;i<size;i++){
+      items.push(false);
+    }
+    setHoveredItems(items)
 
-    let strokeArray=[];
+    sortedValues = initialValues.sort((a, b) => b - a);
+    dataTotal = sortedValues.reduce((acc, val) => acc + val);
+    sortedValues.forEach((dataVal, index) => {
+      chartData.push(angleOffset);
+      angleOffset = (dataVal / dataTotal) * 360 + angleOffset;
+
+      console.log(hoveredItems)
+    });
+    setChartValues(sortedValues);
+    let strokeArray = [];
     sortedValues.forEach((data, index) => {
-      const strokeDirr = data / dataTotal * circumference;
-      console.log(strokeDirr)
+      const strokeDirr = (data / dataTotal) * circumference;
+      console.log(strokeDirr);
       strokeArray.push(circumference - strokeDirr);
-    })
+    });
     setStrokeDashOffset(strokeArray);
 
     let rotateArray = [];
     sortedValues.forEach((data, index) => {
-      const rotate = `rotate(${chartData[index]}, ${cx}, ${cy})`
-      rotateArray.push(rotate)
-    })
-    setCircleTransform([...rotateArray])
-    console.log(rotateArray)
+      const rotate = `rotate(${chartData[index]}, ${cx}, ${cy})`;
+      rotateArray.push(rotate);
+    });
+    setCircleTransform([...rotateArray]);
+
+    console.log(rotateArray);
     return () => {
-      console.log('cleanup');
+      console.log("cleanup");
     };
-  }, []);
+  }, [initialValues]);
+
+  const handleCurrentItemHover = (index) => {
+    console.log(" On mouse enter")
+    let items = hoveredItems;
+    items[index] = true;
+    setHoveredItems([...items]);
+  };
+
+  const handleCurrentItemHoverOff = (index) => {
+    let items = hoveredItems;
+    items[index] = false;
+    setHoveredItems([...items]);
+    console.log("mouse leave")
+  };
 
 
   return (
     <div>
-      <svg height="250" width="200" viewBox="0 0 200 200">
+      <svg height="300" width="300" viewBox="0 0 300 300">
         {strokeDashOffset.map((value, index) => (
-          <g>
+          <g key={index+value}>
             <circle
+              onMouseEnter={()=>handleCurrentItemHover(index)}
+              onMouseLeave={()=>handleCurrentItemHoverOff(index)}
               cx={cx}
               cy={cy}
               r={radius}
@@ -58,13 +98,19 @@ export const PieChart2 = ({ initialValues = [15, 31, 221, 5, 15] }) => {
               strokeWidth={strokeWidth}
               strokeDasharray={adjustedCircumference}
               strokeDashoffset={value}
-              transform={circleTransform[index]}
+              transform={hoveredItems[index] ? `${circleTransform[index]} scale(1.1)`: `${circleTransform[index]}`}
               fill="transparent"
+              //style={{stroke: hoveredItems[index] ? "black": "white" }}
             />
-            <text x="80" y="80">test</text>
+
+            <text style={{ color: "#fff" }} x={cx} y={cy}>
+              {chartValues[index]}
+            </text>
           </g>
         ))}
       </svg>
+      Hovers
+      {hoveredItems.map((val)=>{val.toString()})}
     </div>
   );
 };
